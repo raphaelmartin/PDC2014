7b68787e21450256c27cdc808386f1ac7312b20c
raphaelmartin <pinkcurls@me.com>
2014-07-19 15:26:12 -0500

    moment.js 1st try

---
+++ moment_test.js
@@ -0,0 +1,10 @@
+//moment().tz("America/Los_Angeles").format("h:mm a")
+var now = moment();
+
+$('time').each(function(i, e) {
+    var time = moment($(e).attr('datetime'));
+
+    if(now.diff(time, 'days') <= 1) {
+        $(e).html('<span>' + time.from(now) + '</span>');
+    }
+});
